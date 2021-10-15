ca6789e478faf54aa8cbf6c6108faea7d1cd8a66
fconzelmann <federico.conzelma@epfl.ch>
2021-10-15 13:39:17 +0200

    Added qolo model and launch file

---
+++ objects_descriptions/launch/qolo.launch.py
@@ -0,0 +1,43 @@
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("objects_descriptions"), "urdf/qolo.urdf.xacro"]),
+            " ",
+            "prefix:=qolo_ ",
+            "connected_to:='' ",
+            "xyz:='1 0 0' ",
+            "rpy:='0 0 0' ",
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", PathJoinSubstitution([FindPackageShare("objects_descriptions"), "rviz/object.rviz"])],
+        output="log",
+    )
+
+    nodes = [
+        robot_state_pub_node,
+        rviz_node,
+    ]
+
+    return LaunchDescription(nodes)
