2ffa9483f9ca2abdb416bb3bfc3f5334af28d7d9
Lukas Huber <hubernikus@gmail.com>
2023-01-31 17:20:27 +0100

    Optional docker launch (as comment)

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker build -t ros2_autonomous_furniture .
 
+# DOCKER_BUILDKIT=1 docker build --build-arg HOST_GID=$(id -g) --build-arg HOST_UID=$(id -u)
 # install ros package
 
 # TO run:
